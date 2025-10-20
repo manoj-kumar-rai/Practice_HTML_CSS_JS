8810b1a35aefcf4ed1168a77ed5cc817bc21416e
Manoj Kumar Rai <iammanojkumarrai@gmail.com>
2025-10-20 05:13:15 +0000

    Practiced Call Back Hell

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,15 @@
-console.log("Manoj");
+function hello(getData, getNextData) {
+    setTimeout(() => {
+        console.log("Data is: ", getData);
+     if(getNextData) {
+        getNextData();
+    }
+}, 2000)};
+
+    hello(1, () => {
+        console.log("Data2 is loading......");   
+    hello(2, () => {
+        console.log("Data3 is loading......");
+    })
+    } 
+);
